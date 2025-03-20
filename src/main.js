a6e44a1644a1aa0935fb9b6527f6a83af74b0aa9
Anderson Salviano <andyanodev@gmail.com>
2025-03-19 21:30:17 -0300

    criando a seção de planos

---
+++ src/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const tabTarget = botao.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${tabTarget}]`);
+            hideAllTabs();
+            tab.classList.add('shows__list--is-active');
+            removeButtonActive();
+            botao.target.classList.add('shows__tabs__button--is-active');
+        })
+    }
+})
+
+function removeButtonActive() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active')
+    }
+}
+
+function hideAllTabs() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
